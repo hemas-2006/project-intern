d9df969316c9732df808b1c72d1184f504fe61e3
hemas-2006 <2403717620522016@cit.edu.in>
2026-06-06 13:47:05 +0530

    backend-final

---
+++ frontend_final/src/pages/Login.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+
+import axios from "axios";
+
+function Login() {
+
+  const [email, setEmail] =
+  useState("");
+
+  const [password, setPassword] =
+  useState("");
+
+  async function handleLogin(e){
+
+    e.preventDefault();
+
+    try {
+
+      const response =
+      await axios.post(
+
+        "http://localhost:5000/api/auth/login",
+
+        {
+          email,
+          password
+        }
+
+      );
+
+      alert(
+        response.data.message
+      );
+
+      console.log(
+        response.data
+      );
+
+    }
+
+    catch(error){
+
+      alert(
+        "Login Failed"
+      );
+
+      console.log(error);
+
+    }
+  }
+
+  return (
+
+    <div style={{padding:"30px"}}>
+
+      <h2>
+        Login Page
+      </h2>
+
+      <form onSubmit={handleLogin}>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e)=>
+          setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e)=>
+          setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+
+          Login
+
+        </button>
+
+      </form>
+
+    </div>
+  );
+}
+
+export default Login;
